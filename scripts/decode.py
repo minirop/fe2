f17cd756262366a1822c0ea6495f0889a40ae4b6
minirop <minirop@hotmail.com>
2025-06-06 20:49:03 +0200

    Many (all?) dialogue strings converted

--- scripts/decode.py
+++ scripts/decode.py
@@ -25,9 +25,16 @@ with open(f'{dir_path}/kana.tbl', 'r') as f:
 
 def decode(code):
     c = code.split(' ')
-    output = ["ENCODE(\""]
-    in_string = True
+    output = []
+    in_string = False
+    in_e8 = False
     for cc in c:
+        if in_e8:
+            if cc != 'EA':
+                output += f" ${cc}"
+                continue
+            else:
+                in_e8 = False
         match cc:
             case '0F':
                 output[-1] = tenten[output[-1]]
@@ -48,7 +55,24 @@ def decode(code):
                     if in_string:
                         in_string = False
                         output += "\")"
-                    output += f" ${cc}"
+                    match cc:
+                        case 'E6':
+                            output += "\n.db $E6\n.db"
+                        case 'E8':
+                            output += " $E8"
+                            in_e8 = True
+                        case 'EA':
+                            output += "\n.db SPEAK\n.db"
+                        case 'ED':
+                            output += "\n.db NEWLINE\n.db"
+                        case 'EE':
+                            output += "\n.db WAIT_CLICK\n.db"
+                        case 'EF':
+                            output += "\n.db $EF\n"
+                        case 'F0':
+                            output += "\n.db $F0\n.db"
+                        case _:
+                            output += f" ${cc}"
 
     if in_string:
         output += "\")"
